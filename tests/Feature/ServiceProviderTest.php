800476dfe726757f43bbdc46b3e6c49138dbfcda
Philipp Daun <post@philippdaun.net>
2024-11-26 17:43:05 +0100

    Adjust provider tests
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- tests/Feature/ServiceProviderTest.php
+++ tests/Feature/ServiceProviderTest.php
@@ -2,17 +2,15 @@
 
 use Daun\StatamicLoupe\Loupe\Index;
 use Daun\StatamicLoupe\ServiceProvider;
-use Loupe\Loupe\Loupe;
 use Statamic\Facades\Search;
 
-test('boots without issues', function () {
+it('boots without issues', function () {
     $provider = new ServiceProvider($this->app);
     $provider->bootAddon();
 })->throwsNoExceptions();
 
-test('register a search driver', function () {
+it('register a search driver', function () {
     $index = Search::index('loupe_index');
 
     expect($index)->toBeInstanceOf(Index::class);
-    expect($index->client())->toBeInstanceOf(Loupe::class);
 });

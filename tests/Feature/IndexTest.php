71659704e1d29d61dca72db93aebf2858c3ae93a
Philipp Daun <post@philippdaun.net>
2024-11-26 19:41:33 +0100

    Finish index tests
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- tests/Feature/IndexTest.php
+++ tests/Feature/IndexTest.php
@@ -5,6 +5,10 @@ use Statamic\Facades\Collection;
 use Statamic\Facades\Entry;
 use Statamic\Facades\Search;
 
+beforeEach(function () {
+    config(['statamic.search.drivers.loupe.path' => fixtures_path('indexes/' . random_int(11, 99999999))]);
+});
+
 it('creates a Loupe client', function () {
     $index = Search::index('loupe_index');
 
@@ -12,6 +16,10 @@ it('creates a Loupe client', function () {
 });
 
 it('adds documents to the index', function () {
+    $index = Search::index('loupe_index');
+
+    $this->assertCount(0, $index->lookup('Entry'));
+
     $collection = Collection::make()
         ->handle('pages')
         ->title('Pages')
@@ -30,13 +38,13 @@ it('adds documents to the index', function () {
     $entry2->save();
 
     sleep(1); // give it some time to process
-
-    $index = Search::index('loupe_index');
 
     $this->assertCount(2, $index->lookup('Entry'));
 });
 
 it('updates documents in the index', function () {
+    $index = Search::index('loupe_index');
+
     $collection = Collection::make()
         ->handle('pages')
         ->title('Pages')
@@ -55,8 +63,6 @@ it('updates documents in the index', function () {
     $entry2->save();
 
     sleep(1); // give it some time to process
-
-    $index = Search::index('loupe_index');
 
     $results = collect($index->lookup('Entry'))->pluck('title');
 
@@ -70,4 +76,41 @@ it('updates documents in the index', function () {
     $results = collect($index->lookup('Entry'))->pluck('title');
 
     $this->assertContains('Entry 2 Updated', $results);
+});
+
+it('removes documents from the index', function () {
+    $index = Search::index('loupe_index');
+
+    $collection = Collection::make()
+        ->handle('pages')
+        ->title('Pages')
+        ->save();
+
+    $entry1 = Entry::make()
+        ->id('test-1')
+        ->collection('pages')
+        ->data(['title' => 'Entry 1']);
+    $entry1->save();
+
+    $entry2 = Entry::make()
+        ->id('test-2')
+        ->collection('pages')
+        ->data(['title' => 'Entry 2']);
+    $entry2->save();
+
+    sleep(1); // give it some time to process
+
+    $results = collect($index->lookup('Entry'))->pluck('title');
+
+    $this->assertContains('Entry 1', $results);
+    $this->assertContains('Entry 2', $results);
+
+    $entry2->delete();
+
+    sleep(1); // give it some time to process
+
+    $results = collect($index->lookup('Entry'))->pluck('title');
+
+    $this->assertContains('Entry 1', $results);
+    $this->assertNotContains('Entry 2', $results);
 });

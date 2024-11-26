7456ec965868ee791bcbf50dbc857c650af04e0d
Philipp Daun <post@philippdaun.net>
2024-11-26 20:17:56 +0100

    Get tests for snippets working
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- tests/Feature/SnippetsTest.php
+++ tests/Feature/SnippetsTest.php
@@ -11,20 +11,20 @@ it('returns empty string for empty input', function () {
 
 it('returns empty string when no start tag is found', function () {
     $snippets = new Snippets();
-    expect($snippets->generate('This is a test string without start tag.'))->toEqual('');
+    expect($snippets->generate('Lorem ipsum dolor sit amet.'))->toEqual('');
 });
 
-it('returns snippet with single match', function () {
+it('returns snippet with single match unchanged', function () {
     $snippets = new Snippets();
-    $text = 'This is a <mark>test</mark> string.';
-    $expected = 'This is a <mark>test</mark> string.';
+    $text = 'Lorem ipsum dolor sit amet, <mark>consectetur</mark> adipiscing elit.';
+    $expected = 'Lorem ipsum dolor sit amet, <mark>consectetur</mark> adipiscing elit.';
     expect($snippets->generate($text))->toEqual($expected);
 });
 
-it('returns snippet with multiple matches', function () {
+it('returns snippet with multiple matches unchanged', function () {
     $snippets = new Snippets();
-    $text = 'This is a <mark>test</mark> string with another <mark>match</mark>.';
-    $expected = 'This is a <mark>test</mark> string with another <mark>match</mark>.';
+    $text = 'Lorem ipsum <mark>dolor</mark> sit amet, <mark>consectetur</mark> adipiscing elit.';
+    $expected = 'Lorem ipsum <mark>dolor</mark> sit amet, <mark>consectetur</mark> adipiscing elit.';
     expect($snippets->generate($text))->toEqual($expected);
 });
 
@@ -35,16 +35,44 @@ it('returns snippet with surrounding words', function () {
     expect($snippets->generate($text))->toEqual($expected);
 });
 
-it('returns snippet with separator for distant matches', function () {
+it('truncates space between distant matches', function () {
     $snippets = new Snippets();
-    $text = 'This is a <mark>test</mark> string. And here is another <mark>match</mark> far away.';
-    $expected = 'This is a <mark>test</mark> string. ... And here is another <mark>match</mark> far away.';
+    $text = 'Lorem ipsum <mark>dolor</mark> sit amet consectetur adipiscing dolor sit elit lorem amet ipsum dolor sit amet <mark>consectetur</mark> adipiscing elit.';
+    $expected = 'Lorem ipsum <mark>dolor</mark> sit amet consectetur adipiscing dolor ... amet ipsum dolor sit amet <mark>consectetur</mark> adipiscing elit.';
     expect($snippets->generate($text))->toEqual($expected);
 });
 
-it('handles custom tags and separator', function () {
-    $snippets = new Snippets('[start]', '[end]', 4, '---');
-    $text = 'This is a [start]test[end] string with a custom separator.';
-    $expected = 'This is a [start]test[end] string with a custom separator.';
+it('truncates space before first match', function () {
+    $snippets = new Snippets();
+    $text = 'Lorem ipsum dolor sit amet consectetur adipiscing <mark>dolor</mark> sit';
+    $expected = '... dolor sit amet consectetur adipiscing <mark>dolor</mark> sit';
+    expect($snippets->generate($text))->toEqual($expected);
+});
+
+it('truncates space after first match', function () {
+    $snippets = new Snippets();
+    $text = 'Lorem ipsum dolor <mark>sit</mark> amet consectetur adipiscing dolor sit amet consectetur adipiscing';
+    $expected = 'Lorem ipsum dolor <mark>sit</mark> amet consectetur adipiscing dolor sit ...';
+    expect($snippets->generate($text))->toEqual($expected);
+});
+
+it('allow customizing the number of words', function () {
+    $snippets = new Snippets(surroundingWords: 3);
+    $text = 'Lorem ipsum <mark>dolor</mark> sit amet consectetur adipiscing dolor sit elit lorem amet ipsum dolor sit amet <mark>consectetur</mark> adipiscing elit.';
+    $expected = 'Lorem ipsum <mark>dolor</mark> sit amet consectetur ... dolor sit amet <mark>consectetur</mark> adipiscing elit.';
+    expect($snippets->generate($text))->toEqual($expected);
+});
+
+it('handles custom tag', function () {
+    $snippets = new Snippets('[start]', '[end]');
+    $text = 'Lorem ipsum dolor sit amet consectetur adipiscing [start]dolor[end] sit';
+    $expected = '... dolor sit amet consectetur adipiscing [start]dolor[end] sit';
+    expect($snippets->generate($text))->toEqual($expected);
+});
+
+it('handles custom separator', function () {
+    $snippets = new Snippets(separator: '???');
+    $text = 'Lorem ipsum dolor sit amet consectetur adipiscing <mark>dolor</mark> sit';
+    $expected = '??? dolor sit amet consectetur adipiscing <mark>dolor</mark> sit';
     expect($snippets->generate($text))->toEqual($expected);
 });

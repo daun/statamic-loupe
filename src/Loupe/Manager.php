2fa52725ef8de420b577635e9f26f5966ce1aa6a
Philipp Daun <post@philippdaun.net>
2024-11-26 16:39:57 +0100

    Refactor client manager
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- src/Loupe/Manager.php
+++ src/Loupe/Manager.php
@@ -2,11 +2,13 @@
 
 namespace Daun\StatamicLoupe\Loupe;
 
-use Illuminate\Support\Facades\File;
+use Exception;
+use Illuminate\Filesystem\Filesystem;
 use Loupe\Loupe\Configuration;
 use Loupe\Loupe\Loupe;
 use Loupe\Loupe\LoupeFactory;
-use Statamic\Facades\Path;
+
+use function Illuminate\Filesystem\join_paths;
 
 class Manager
 {
@@ -15,16 +17,33 @@ class Manager
      */
     protected array $clients = [];
 
+    protected LoupeFactory $factory;
+
+    protected Filesystem $filesystem;
+
     public function __construct(
-        protected readonly LoupeFactory $factory,
-        protected string $path,
+        protected string $path
     ) {
-        File::ensureDirectoryExists($this->path, mode: 0777);
+        $this->factory = new LoupeFactory();
+        $this->filesystem = new Filesystem();
+        $this->init();
+    }
+
+    protected function init(): void
+    {
+        $this->filesystem->ensureDirectoryExists($this->path);
+
+        if (! $this->filesystem->isDirectory($this->path)) {
+            throw new Exception(sprintf('The Loupe path "%s" does not exist and cannot be created.', $this->path));
+        }
+        if (! $this->filesystem->isWritable($this->path)) {
+            throw new Exception(sprintf('The Loupe path "%s" is not writable.', $this->path));
+        }
     }
 
     public function get(string $index, Configuration $configuration): Loupe
     {
-        return ($this->clients[$index] ??= $this->make($index, $configuration));
+        return $this->clients[$index] ??= $this->make($index, $configuration);
     }
 
     public function make(string $index, Configuration $configuration): Loupe
@@ -34,31 +53,43 @@ class Manager
         return $this->factory->create($this->indexDirectory($index), $configuration);
     }
 
-    public function indexDirectory(string $index): string
-    {
-        return Path::resolve("{$this->path}/{$index}");
-    }
-
-    public function indexPath(string $index): string
-    {
-        return Path::resolve("{$this->path}/{$index}/loupe.db");
-    }
-
     public function indexExists(string $index): bool
     {
-        return File::exists($this->indexPath($index));
+        return $this->filesystem->exists($this->indexPath($index));
     }
 
     public function createIndex(string $index): void
     {
-        File::ensureDirectoryExists($this->indexDirectory($index), mode: 0777);
-        if (! File::exists($db = $this->indexPath($index))) {
-            File::put($db, '', lock: true);
+        $dir = $this->indexDirectory($index);
+        $db = $this->indexPath($index);
+
+        if (! $this->filesystem->exists($db)) {
+            $this->filesystem->ensureDirectoryExists($dir);
+            $this->filesystem->put($db, '');
+        }
+
+        if (! $this->filesystem->isFile($db)) {
+            throw new Exception(sprintf('The Loupe index "%s" does not exist and cannot be created.', $db));
+        }
+
+        if (! $this->filesystem->isWritable($db)) {
+            throw new Exception(sprintf('The Loupe index "%s" is not writable.', $db));
         }
     }
 
     public function dropIndex(string $index): void
     {
-        File::cleanDirectory($this->indexDirectory($index));
+        $this->get($index, Configuration::create())->deleteAllDocuments();
+        // $this->filesystem->cleanDirectory($this->indexDirectory($index));
+    }
+
+    public function indexDirectory(string $index): string
+    {
+        return join_paths($this->path, $index);
+    }
+
+    public function indexPath(string $index): string
+    {
+        return join_paths($this->path, $index, 'loupe.db');
     }
 }
